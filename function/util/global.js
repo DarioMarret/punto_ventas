c011ca03036a21ffb1d57e22a8dc8a0781f53a0b
DarioMarret <64284053+DarioMarret@users.noreply.github.com>
2022-04-17 10:07:11 -0500

    first commit

--- function/util/global.js
+++ function/util/global.js
@@ -1,28 +1,6 @@
 export const usuario_local = "usuario:";
 export const usuario_token = "token_usuario:";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //localinfo
 export const DOCUMENTO = [
     {
@@ -33,79 +11,81 @@ export const DOCUMENTO = [
       "forma":"Ticket",
       "codigo":"00",
     }
-  ]
-  export const FORMAPAGO = [
-    {
-      "E":"EFECTIVO",
-      "M":"EFECTIVO"
-    },
-    {
-      "E":"TARJETA",
-      "M":"TARJETA"
-    },
-    {
-      "E":"TRANSFERENCIA",
-      "M":"TRANSFERENCIA"
-    }
-  ]
-  export const ESTADO = [
-    {
-      "E":"ACTIVO",
-      "M":"ACTIVO"
-    },
-    {
-      "E":"ANULADO",
-      "M":"ANULADO"
-    }
-  ]
+]
+  
+export const FORMAPAGO = [
+  {
+    "E":"EFECTIVO",
+    "M":"EFECTIVO"
+  },
+  {
+    "E":"TARJETA",
+    "M":"TARJETA"
+  },
+  {
+    "E":"TRANSFERENCIA",
+    "M":"TRANSFERENCIA"
+  }
+]
 
-  export const columns = [
-    {
-     name: "secuencia",
-     label: "SECUENCIAL",
-     options: {
-      filter: true,
-      sort: true,
-     }
-    },
-    {
-     name: "forma_pago",
-     label: "FORMA DE PAGO",
-     options: {
+export const ESTADO = [
+  {
+    "E":"ACTIVO",
+    "M":"ACTIVO"
+  },
+  {
+    "E":"ANULADO",
+    "M":"ANULADO"
+  }
+]
+
+export const columns = [
+  {
+    name: "secuencia",
+    label: "SECUENCIAL",
+    options: {
+    filter: true,
+    sort: true,
+    }
+  },
+  {
+    name: "forma_pago",
+    label: "FORMA DE PAGO",
+    options: {
+    filter: true,
+    sort: false,
+    }
+  },
+  {
+    name: "total",
+    label: "TOTAL",
+    options: {
+    filter: true,
+    sort: false,
+    }
+  },
+  {
+    name: "fecha_creacion",
+    label: "FECHA",
+    options: {
+    filter: true,
+    sort: false,
+    }
+  },
+  {
+    name: "empresa",
+    label: "EMPRESA",
+    options: {
       filter: true,
       sort: false,
-     }
-    },
+    }
+    },    
     {
-     name: "total",
-     label: "TOTAL",
-     options: {
+    name: "estado",
+    label: "ESTADO",
+    options: {
       filter: true,
       sort: false,
-     }
+    }
     },
-    {
-     name: "fecha_creacion",
-     label: "FECHA",
-     options: {
-      filter: true,
-      sort: false,
-     }
-    },
-    {
-      name: "empresa",
-      label: "EMPRESA",
-      options: {
-       filter: true,
-       sort: false,
-      }
-     },    
-     {
-      name: "estado",
-      label: "ESTADO",
-      options: {
-       filter: true,
-       sort: false,
-      }
-     },
-   ];
+]
